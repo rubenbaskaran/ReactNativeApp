0d51d4076971aacb41dac41c59702ea2ca669b4a
Ruben Baskaran <rbaskaran92@gmail.com>
2020-10-12 23:33:40 +0200

    added costumization for TextInput component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Text, StyleSheet, Button, View } from "react-native";
+import { Text, StyleSheet, Button, View, TextInput } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
   const [counter, setCounter] = useState(0);
+  const [name, setName] = useState("");
 
   return (
     <View>
@@ -24,6 +25,15 @@ const HomeScreen = ({ navigation }) => {
           setCounter(counter - 1);
         }}
       ></Button>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
+      ></TextInput>
+      <Text>Your name is: {name}</Text>
+      {name.match(/\d/) ? <Text>Error! Name can't contain numbers</Text> : null}
     </View>
   );
 };
@@ -31,6 +41,12 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+  },
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1,
+    color: "black",
   },
 });
 
