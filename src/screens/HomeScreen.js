1334fa5f5cea6594e1e47b40dbf8eabd2e2136a9
Ruben Baskaran <rbaskaran92@gmail.com>
2020-10-12 19:11:31 +0200

    added counter for home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, StyleSheet, Button, View } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <View>
       <Text style={styles.text}>Home</Text>
       <Button
         title="Go to About"
         onPress={() => navigation.navigate("About")}
+      ></Button>
+      <Text>Current Count: {counter}</Text>
+      <Button
+        title="Increase"
+        onPress={() => {
+          setCounter(counter + 1);
+        }}
+      ></Button>
+      <Button
+        title="Decrease"
+        onPress={() => {
+          setCounter(counter - 1);
+        }}
       ></Button>
     </View>
   );
