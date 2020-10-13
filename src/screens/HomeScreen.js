044bdfb60f773531e3a2bd436a0d9916ae0372d8
Ruben Baskaran <rbaskaran92@gmail.com>
2020-10-13 17:25:28 +0200

    did a lot of layout twerking just for fun

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -14,41 +14,89 @@ const HomeScreen = ({ navigation }) => {
         flexDirection: "column",
         justifyContent: "flex-start",
         height: "100%",
+        backgroundColor: "black",
       }}
     >
-      <Text style={styles.text}>Home</Text>
-      <Button
-        title="Go to About"
-        onPress={() => navigation.navigate("About")}
-      ></Button>
-      <View style={{ alignItems: "center" }}>
-        <Text>Current Count: {counter}</Text>
-      </View>
-      <Button
-        title="Increase"
-        onPress={() => {
-          setCounter(counter + 1);
+      <View
+        style={{
+          flexDirection: "column",
+          position: "absolute",
+          zIndex: 0,
+          height: "100%",
+          width: "100%",
+          borderWidth: 1,
+          borderColor: "blue",
         }}
-      ></Button>
-      <Button
-        title="Decrease"
-        onPress={() => {
-          setCounter(counter - 1);
-        }}
-      ></Button>
-      <TextInput
-        style={styles.input}
-        autoCapitalize="none"
-        autoCorrect={false}
-        value={name}
-        onChangeText={(newValue) => setName(newValue)}
-      ></TextInput>
-      <View style={{ alignItems: "center" }}>
-        <Text>Your name is: {name}</Text>
-        {name.match(/\d/) ? (
-          <Text>Error! Name can't contain numbers</Text>
-        ) : null}
+      >
+        <View style={{ flex: 4.75, backgroundColor: "green" }}></View>
+        <View
+          style={{
+            flex: 0.5,
+            backgroundColor: "blue",
+            flexDirection: "column",
+            top: 10,
+            zIndex: 2,
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: "red",
+              width: "20%",
+              height: "100%",
+              alignSelf: "center",
+            }}
+          ></View>
+        </View>
+        <View style={{ flex: 4.75, backgroundColor: "orange" }}></View>
       </View>
+      <View
+        style={{
+          flex: 2,
+          borderWidth: 5,
+          borderColor: "yellow",
+        }}
+      >
+        <Text style={styles.text}>Home</Text>
+        <Button
+          title="Go to About"
+          onPress={() => navigation.navigate("About")}
+        ></Button>
+        <View>
+          <Text style={{ alignSelf: "center" }}>Current Count: {counter}</Text>
+        </View>
+        <Button
+          title="Increase"
+          onPress={() => {
+            setCounter(counter + 1);
+          }}
+        ></Button>
+        <Button
+          title="Decrease"
+          onPress={() => {
+            setCounter(counter - 1);
+          }}
+        ></Button>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          autoCorrect={false}
+          value={name}
+          onChangeText={(newValue) => setName(newValue)}
+        ></TextInput>
+        <View style={{ alignItems: "center" }}>
+          <Text>Your name is: {name}</Text>
+          {name.match(/\d/) ? (
+            <Text>Error! Name can't contain numbers</Text>
+          ) : null}
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          borderWidth: 5,
+          borderColor: "green",
+        }}
+      ></View>
     </View>
   );
 };
