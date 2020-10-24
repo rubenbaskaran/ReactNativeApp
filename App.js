ddcee4e66e2fc4da92abe7a6842b5ce379a78105
Ruben Baskaran <rbaskaran92@gmail.com>
2020-10-24 14:33:01 +0200

    wrapped parent component around another component

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import AboutScreen from "./src/screens/AboutScreen";
+import React from "react";
 
 const navigator = createStackNavigator(
   {
@@ -16,4 +17,7 @@ const navigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+export default () => {
+  return <App />;
+};
