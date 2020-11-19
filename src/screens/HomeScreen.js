a26fef7d2df03e6ecc4bf1332ce917f241104ef5
Ruben Baskaran <rbaskaran92@gmail.com>
2020-11-19 23:14:29 +0100

    commented axios request

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -15,7 +15,7 @@ const HomeScreen = ({ navigation }) => {
 
   useEffect(() => {
     const listener = navigation.addListener("didFocus", () => {
-      callWebApi();
+      // callWebApi();
     });
 
     return () => {
